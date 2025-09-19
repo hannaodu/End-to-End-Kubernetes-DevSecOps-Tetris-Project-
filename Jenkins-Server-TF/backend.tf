d04fb3265c2b8902e65785db77214bc5dbbe2b5c
hanna <tennyalana@yahoo.com>
2025-09-19 23:52:11 +0100

    first commit

---
+++ Jenkins-Server-TF/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-aws-jekins"
+    region         = "eu-west-2"
+    key            = "Jenkins-server/terraform.tfstate"
+    use_lockfile   = true
+    encrypt        = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
